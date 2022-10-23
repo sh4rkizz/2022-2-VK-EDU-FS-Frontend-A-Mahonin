fc463c1dcdeadbafcc52679e8d3015a3047095fe
sharkizz <sharkizz.mirea@gmail.com>
2022-10-23 23:44:06 +0300

    Homework #3

---
+++ messenger/webpack.config.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+const SRC_PATH = path.resolve(__dirname, 'src');
+const BUILD_PATH = path.resolve(__dirname, 'build');
+
+module.exports = {
+    context: SRC_PATH,
+    entry: {
+        home: './im/im.js',
+        chat: './chat/chat.js'
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: '[name]-bundle.js'
+    },
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.js$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        },
+                    },
+                ],
+            },
+            {
+                test: /shadow\.css$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'css-loader'
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: '[name]-style.css',
+        }),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: './im/im.html',
+            chunks: ['home']
+        }),
+        new HTMLWebpackPlugin({
+            filename: 'chat.html',
+            template: './chat/chat.html',
+            chunks: ['chat']
+        })
+    ]
+};
