ea6e5d70e78726a9e3b471372871bc65ffea51c6
sharkizz <sharkizz.mirea@gmail.com>
2022-12-29 17:25:52 +0300

    fixed login (almost perfect)

--- react-messenger/src/Components/Atoms/Button/Button.jsx
+++ react-messenger/src/Components/Atoms/Button/Button.jsx
@@ -1,49 +1,32 @@
 import './Button.scss'
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
+import React from 'react'
 
-export function Button({hrefTo, className, name, onClick, onMouseDown, onMouseUp}) {
-    if (hrefTo) return (
-        <Link to={hrefTo} style={{textDecoration: 'none'}}>
-            <button className={className}>{name}</button>
-        </Link>
-    )
+export function Button ({ hrefTo, className, name, onClick, onMouseDown, onMouseUp }) {
+  if (hrefTo) return (
+    <Link to={ hrefTo } style={ { textDecoration: 'none' } }>
+      <button className={ className }>{ name }</button>
+    </Link>
+  )
 
-    return <button className={className} onMouseDown={onMouseDown} onMouseUp={onMouseUp}
-                   onClick={onClick}>{name}</button>
+  return <button className={ className } onMouseDown={ onMouseDown } onMouseUp={ onMouseUp }
+                 onClick={ onClick }>{ name }</button>
 }
 
 export const LoginButton = () => {
-    // const handleCallback = (response) => {
-    //     let user = jwtDecode(response.credential)
-    //     console.log(user.email)
-    // }
-    //
-    // useEffect(() => {
-    //     /* global google */
-    //     google.accounts.id.initialize({
-    //         client_id: '852169726556-11ubfhtu1bgkfimlsof7i555h1i46v6t.apps.googleusercontent.com',
-    //         callback: handleCallback
-    //     })
-    //
-    //     google.accounts.id.renderButton(
-    //         document.getElementById('login-button'),
-    //         {theme: 'outline', size: 'big'}
-    //     )
-    // }, [])
-
-    return (
-        <a href='http://localhost:8000/auth/login/google-oauth2/' style={{textDecoration: 'none'}}>
-            <button className='login-button'>Login with google</button>
-        </a>
-    )
+  return (
+    <a href="http://localhost:8000/auth/login/google-oauth2/" style={ { textDecoration: 'none' } }>
+      <button className="login-button">Login with google</button>
+    </a>
+  )
 }
 
 export const LogoutButton = () => {
-    return (
-        <a href='http://localhost:8000/logout/' style={{textDecoration: 'none'}}>
-            <button className='logout-button'>Logout</button>
-        </a>
-    )
+  return (
+    <a href="http://localhost:8000/logout/" style={ { textDecoration: 'none' } }>
+      <button className="logout-button">Logout</button>
+    </a>
+  )
 }
